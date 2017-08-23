9c1a1dbad5229c95e76ecc9e53a1e203c57f8621
Cameron McQuinn <cameron.mcquinn@gmail.com>
2017-08-22 23:04:10 -0700

    updated FWID python file

--- HW/firmware-tag/FWID_REPLICOOKIE.py
+++ HW/firmware-tag/FWID_REPLICOOKIE.py
@@ -8,29 +8,16 @@ def gen_fwid(*args,**kwargs):
     fw.build_sha = get_git_revision_short_hash()
     fw.comment = get_build_url()
 
+    # Of the 5 LEDs on the Snickerdoodle board,
+    # 1 is accessable for user applications
     fw.fpga_part_number = "7z020"
-    fw.num_leds = 5
+    fw.num_leds = 1
     fw.board_name = "Replicookie"
 
+    # The Snickerdoodle black has 5 FPGA connectors,
+    # but only 1 of them is used for HM2 stuff
     c = fw.connector.add()
     c.name = "JA.GPIO1"
-    c.pins = 25
-
-    c = fw.connector.add()
-    c.name = "JA.GPIO2"
-    c.pins = 25
-
-    c = fw.connector.add()
-    c.name = "JB.GPIO1"
-    c.pins = 25
-
-    c = fw.connector.add()
-    c.name = "JB.GPIO2"
-    c.pins = 25
-
-    c = fw.connector.add()
-    c.name = "JC.GPIO1"
-    c.pins = 25
-
+    c.pins = 40
 
     return fw
