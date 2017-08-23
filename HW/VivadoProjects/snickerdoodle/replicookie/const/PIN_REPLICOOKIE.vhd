92d24bdbad9f1932e33c316938531348093e82f1
Cameron McQuinn <cameron.mcquinn@gmail.com>
2017-08-22 20:49:59 -0600

    added some documentation to the replicookie firmware file

--- HW/VivadoProjects/snickerdoodle/replicookie/const/PIN_REPLICOOKIE.vhd
+++ HW/VivadoProjects/snickerdoodle/replicookie/const/PIN_REPLICOOKIE.vhd
@@ -6,53 +6,57 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 -- Copyright (C) 2007, Peter C. Wallace, Mesa Electronics
 -- http://www.mesanet.com
 --
+-- Ported to Replicookie Snickerdoodle baseboard: 
+-- Copyright (C) 2017, Cameron McQuinn
+-- http://www.github.com/cmcquinn
+--
 -- This program is is licensed under a disjunctive dual license giving you
 -- the choice of one of the two following sets of free software/open source
 -- licensing terms:
 --
 --    * GNU General Public License (GPL), version 2.0 or later
 --    * 3-clause BSD License
--- 
+--
 --
 -- The GNU GPL License:
--- 
+--
 --     This program is free software; you can redistribute it and/or modify
 --     it under the terms of the GNU General Public License as published by
 --     the Free Software Foundation; either version 2 of the License, or
 --     (at your option) any later version.
--- 
+--
 --     This program is distributed in the hope that it will be useful,
 --     but WITHOUT ANY WARRANTY; without even the implied warranty of
 --     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 --     GNU General Public License for more details.
--- 
+--
 --     You should have received a copy of the GNU General Public License
 --     along with this program; if not, write to the Free Software
 --     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
--- 
--- 
+--
+--
 -- The 3-clause BSD License:
--- 
+--
 --     Redistribution and use in source and binary forms, with or without
 --     modification, are permitted provided that the following conditions
 --     are met:
--- 
+--
 --   * Redistributions of source code must retain the above copyright
 --     notice, this list of conditions and the following disclaimer.
--- 
+--
 --   * Redistributions in binary form must reproduce the above
 --     copyright notice, this list of conditions and the following
 --     disclaimer in the documentation and/or other materials
 --     provided with the distribution.
--- 
+--
 --   * Neither the name of Mesa Electronics nor the names of its
 --     contributors may be used to endorse or promote products
 --     derived from this software without specific prior written
 --     permission.
--- 
--- 
+--
+--
 -- Disclaimer:
--- 
+--
 --     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 --     "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 --     LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -65,94 +69,100 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 --     LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 --     ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 --     POSSIBILITY OF SUCH DAMAGE.
--- 
+--
 
 use work.IDROMConst.all;
 
-package REPLICOOKIE is
-	constant ModuleID : ModuleIDType :=( 
-		(WatchDogTag,	x"00",	ClockLowTag,		x"01",	WatchDogTimeAddr&PadT,			WatchDogNumRegs,		x"00",	WatchDogMPBitMask),
-		(IOPortTag,		x"00",	ClockLowTag,		x"02",	PortAddr&PadT,					IOPortNumRegs,			x"00",	IOPortMPBitMask),
-		(StepGenTag,	x"02",	ClockLowTag,		x"0A",	StepGenRateAddr&PadT,			StepGenNumRegs,			x"00",	StepGenMPBitMask),
-		(PWMTag,		x"00",	ClockHighTag,		x"02",	PWMValAddr&PadT,				PWMNumRegs,				x"00",	PWMMPBitMask),
-		(LEDTag,		x"00",	ClockLowTag,		x"01",	LEDAddr&PadT,					LEDNumRegs,				x"00",	LEDMPBitMask),
-		(FWIDTag,       x"00",  ClockLowTag,    	x"01",  FWIDAddr&PadT,        			FWIDNumRegs,            x"00",  FWIDMPBitMask),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000"),
-		(NullTag,		x"00",	NullTag,			x"00",	NullAddr&PadT,					x"00",					x"00",	x"00000000")
-		);
-		
-	
+-- Package description
+-- The "package" declaration is an array of ModuleRecords that defines which HM2 
+-- modules are included in this configuration. The ModuleRecord type is defined in
+-- IDROMConst.vhd, and tells HM2 information about each module such as its address,
+-- pinout, and number of registers.
+
+package PIN_REPLICOOKIE is
+	constant ModuleID : ModuleIDType :=(
+	--	Module			Version No.	Clock			NumInstances	BaseAddr					NumRegisters			Strides		BitMask
+		(WatchDogTag,	x"00",		ClockLowTag,	x"01",			WatchDogTimeAddr&PadT,		WatchDogNumRegs,		x"00",		WatchDogMPBitMask),
+		(StepGenTag,	x"02",		ClockLowTag,	x"08",			StepGenRateAddr&PadT,		StepGenNumRegs,		    x"00",		StepGenMPBitMask),
+		(FWIDTag,       x"00",  	ClockLowTag,    x"01",  		FWIDAddr&PadT,        		FWIDNumRegs,            x"00",  	FWIDMPBitMask),
+		(PWMTag,		x"00",		ClockHighTag,	x"03",			PWMValAddr&PadT,			PWMNumRegs,				x"00",		PWMMPBitMask),
+		(LEDTag,		x"00",		ClockLowTag,	x"01",			LEDAddr&PadT,				LEDNumRegs,				x"00",		LEDMPBitMask),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000"),
+		(NullTag,		x"00",		NullTag,		x"00",			NullAddr&PadT,				x"00",					x"00",		x"00000000")
+		); func 	 sec pin			-- hostmot2 Header  Pin Func
+		IOPortTag & x"00" & StepGenTag & StepGenDirPin,     -- I/O 04	J6      01	A Dir
+		IOPortTag & x"00" & StepGenTag & StepGenStepPin,	-- I/O 05	J6      02	A Step
+		IOPortTag & x"01" & StepGenTag & StepGenDirPin, 	-- I/O 06	J6      03	B Dir
+		IOPortTag & x"01" & StepGenTag & StepGenStepPin,	-- I/O 07	J6      04	B Step
+		IOPortTag & x"02" & StepGenTag & StepGenDirPin,		-- I/O 08	J6      05 	C Dir
+		IOPortTag & x"02" & StepGenTag & StepGenStepPin,	-- I/O 09	J6      06	C Step
+		IOPortTag & x"03" & StepGenTag & StepGenDirPin,		-- I/O 10	J6      07	D Dir
+
+
+
 	constant PinDesc : PinDescType :=(
-		-- 	Base func  sec unit sec func 	 sec pin					-- external DB25
-		IOPortTag & x"00" & NullTag & NullPin,					-- I/O 00	PIN 1		Output 2 just GPIO
-		IOPortTag & x"00" & PWMTag & PWMAOutPin,				-- I/O 01	PIN 14   Spindle DAC PWM
-		IOPortTag & x"00" & StepGenTag & StepGenStepPin,		-- I/O 02	PIN 2    X Step
-		IOPortTag & x"00" & NullTag & NullPin,					-- I/O 03	PIN 15	Fault in just GPIO
-		IOPortTag & x"00" & StepGenTag & StepGenDirPin,			-- I/O 04	PIN 3		X Dir    
-		IOPortTag & x"04" & StepGenTag & StepGenStepPin,		-- I/O 05	PIN 16   Charge Pump (16 KHz)
-		IOPortTag & x"01" & StepGenTag & StepGenStepPin,		-- I/O 06	PIN 4		Y Step
-		IOPortTag & x"00" & NullTag & NullPin,					-- I/O 07	PIN 17	Output 1 just GPIO
-		IOPortTag & x"01" & StepGenTag & StepGenDirPin,			-- I/O 08	PIN 5 	Y Dir
-		IOPortTag & x"02" & StepGenTag & StepGenStepPin,		-- I/O 09	PIN 6		Z Step
-		IOPortTag & x"02" & StepGenTag & StepGenDirPin,			-- I/O 10	PIN 7		Z Dir
-		IOPortTag & x"03" & StepGenTag & StepGenStepPin,		-- I/O 11	PIN 8		A Step
-		IOPortTag & x"03" & StepGenTag & StepGenDirPin,			-- I/O 12	PIN 9		A Dir
-		IOPortTag & x"00" & QCountTag & QCountQAPin,  			-- I/O 13	PIN 10	Input 1 (Quad A)
-		IOPortTag & x"00" & QCountTag & QCountQBPin,  			-- I/O 14	PIN 11	Input 2 (Quad B)
-		IOPortTag & x"00" & QCountTag & QCountIdxPin,    		-- I/O 15	PIN 12	Input 3 (Quad Idx)
-		IOPortTag & x"00" & NullTag & NullPin,	 	   			-- I/O 16	PIN 13	Input 4 just GPIO
+    -- 	Base func  sec unit sec		IOPortTag & x"03" & StepGenTag & StepGenStepPin,	-- I/O 11	J6      08	D Step
+		IOPortTag & x"00" & NullTag & NullPin,				-- I/O 00	U20 SW  01   input switch GPIO
+		IOPortTag & x"00" & NullTag & NullPin,				-- I/O 01	U20 SW  02   input switch GPIO
+		IOPortTag & x"00" & NullTag & NullPin,          	-- I/O 02	U20 SW  03   input switch GPIO
+		IOPortTag & x"00" & NullTag & NullPin,				-- I/O 03	U20 SW  04   input switch GPIO
+		IOPortTag & x"00" & HM2DPLLTag & HM2DPLLRefOutPin,	-- I/O 12	J5      01	DPLL Ref Output
+        IOPortTag & x"00" & QCountTag & QCountQAPin,  		-- I/O 13	J5      02	Input 1 (Quad A)
+		IOPortTag & x"00" & QCountTag & QCountQBPin,  		-- I/O 14	J5      03	Input 2 (Quad B)
+		IOPortTag & x"00" & QCountTag & QCountIdxPin,    	-- I/O 15	J5      04	Input 3 (Quad Idx)
+		IOPortTag & x"00" & NullTag & NullPin,				-- I/O 16	J5      05  GPIO
 		
-																	
-		IOPortTag & x"00" & NullTag & NullPin,					-- I/O 17	PIN 1	PIN 1 	Output 2 just GPIO
-		IOPortTag & x"01" & PWMTag & PWMAOutPin,				-- I/O 18   PIN 2	PIN 14	Spindle DAC PWM
-		IOPortTag & x"05" & StepGenTag & StepGenStepPin,		-- I/O 19   PIN 3	PIN 2	   X Step
-		IOPortTag & x"00" & NullTag & NullPin,					-- I/O 20	PIN 4	PIN 15	Fault in just GPIO
-		IOPortTag & x"05" & StepGenTag & StepGenDirPin,			-- I/O 21	PIN 5	PIN 3	   X Dir    
-		IOPortTag & x"09" & StepGenTag & StepGenStepPin,		-- I/O 22	PIN 6	PIN 16	Charge Pump (16 KHz)
-		IOPortTag & x"06" & StepGenTag & StepGenStepPin,		-- I/O 23	PIN 7	PIN 4	   Y Step
-		IOPortTag & x"00" & NullTag & NullPin,					-- I/O 24	PIN 8	PIN 17	Output 1 just GPIO
-		IOPortTag & x"06" & StepGenTag & StepGenDirPin,			-- I/O 25	PIN 9	PIN 5	   Y Dir
-		IOPortTag & x"07" & StepGenTag & StepGenStepPin,		-- I/O 26	PIN 11	PIN 6	   Z Step
-		IOPortTag & x"07" & StepGenTag & StepGenDirPin,			-- I/O 27	PIN 13	PIN 7	   Z Dir
-		IOPortTag & x"08" & StepGenTag & StepGenStepPin,		-- I/O 28	PIN 15	PIN 8	   A Step
-		IOPortTag & x"08" & StepGenTag & StepGenDirPin,			-- I/O 29	PIN 17	PIN 9	   A Dir
-		IOPortTag & x"01" & QCountTag & QCountQAPin,  			-- I/O 30	PIN 19	PIN 10	Input 1 (Quad A)
-		IOPortTag & x"01" & QCountTag & QCountQBPin,  			-- I/O 31	PIN 21	PIN 11  	Input 2 (Quad B)
-		IOPortTag & x"01" & QCountTag & QCountIdxPin,    		-- I/O 32	PIN 23	PIN 12	Input 3 (Quad Idx)
-		IOPortTag & x"00" & NullTag & NullPin,	 	   			-- I/O 33	PIN 25	PIN 13	Input 4 just GPIO
+    -- 	Base func  sec unit sec func 	 sec pin			-- hostmot2 Header Pin Func
+		IOPortTag & x"00" & PWMTag & PWMAOutPin,			-- I/O 17	J5      06   PWM
+		IOPortTag & x"01" & PWMTag & PWMAOutPin,          	-- I/O 18	J5      07   PWM
+		IOPortTag & x"02" & PWMTag & PWMAOutPin,			-- I/O 19	J5      08   PWM
+		IOPortTag & x"04" & StepGenTag & StepGenDirPin,     -- I/O 20	J3      05	E Dir
+		IOPortTag & x"04" & StepGenTag & StepGenStepPin,	-- I/O 21	J3      06	E Step
+		IOPortTag & x"05" & StepGenTag & StepGenDirPin,	    -- I/O 22	J3      07	F Dir
+		IOPortTag & x"05" & StepGenTag & StepGenStepPin,	-- I/O 23	J3      08	F Step
+		IOPortTag & x"06" & StepGenTag & StepGenDirPin,		-- I/O 24	J3      09 	G Dir
+		IOPortTag & x"06" & StepGenTag & StepGenStepPin,	-- I/O 25	J3      10	G Step
+		IOPortTag & x"07" & StepGenTag & StepGenDirPin,		-- I/O 26	J3      11	H Dir
+		IOPortTag & x"07" & StepGenTag & StepGenStepPin,	-- I/O 27	J3      12	H Step
+		IOPortTag & x"00" & NullTag & NullPin,	            -- I/O 28	J3      13	GPIO
+        IOPortTag & x"01" & QCountTag & QCountQAPin,  		-- I/O 29	J3      14	Input 1 (Quad A)
+		IOPortTag & x"01" & QCountTag & QCountQBPin,  		-- I/O 30	J3      15	Input 2 (Quad B)
+		IOPortTag & x"01" & QCountTag & QCountIdxPin,    	-- I/O 31	J3      16	Input 3 (Quad Idx)
+		IOPortTag & x"00" & NullTag & NullPin,  		    -- I/O 32	J3      17	GPIO
+		IOPortTag & x"00" & NullTag & NullPin,   	        -- I/O 33	J3      18	GPIO
 		
+		-- Fill remaining 144 pins
+        emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin, 
+		emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,
 
-		emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin, -- added for 34 pin 5I25
-		emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,
-
-
-		emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin, -- added for IDROM v3
 		emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,
-					
+		emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,
+
 		emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,
 		emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,
 		emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,
@@ -164,4 +174,4 @@ package REPLICOOKIE is
 		emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,
 		emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin,emptypin);
 
-end package REPLICOOKIE;
+end package PIN_REPLICOOKIE;
